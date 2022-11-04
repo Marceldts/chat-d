679daf1aaef11b6305e5c177e0ca445b378ae8f9
Marceldts <marceldeltoro232@gmail.com>
2022-11-04 11:53:51 +0100

    Añadidos terminos y condiciones

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import { LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { async } from '@firebase/util';
 
 @Component({
   selector: 'app-register',
@@ -11,20 +12,40 @@ import { LoadingController } from '@ionic/angular';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
+  check = document.querySelector('#condition');
+  termsRead = false
 
   constructor(
     private readonly fb: FormBuilder,
     private readonly authService: AuthService,
     private readonly router: Router,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    private alertController: AlertController
   ) {}
 
   ngOnInit() {
-    this.checkForm();
+    this.checkForm().then(() =>this.checkboxListener())
+  }
+
+  async checkboxListener(){
+    const check = document.querySelector('#condition');
+    // if(!this.termsRead) this.check.ariaDisabled
+    check.addEventListener('ionChange', (ev) => {
+      // if(this.termsRead) (<any>this.check).checked = true
+      console.log((<any>ev).detail.checked)
+      if(this.termsRead && (<any>ev).detail.checked){
+        (<any>check).checked = true
+      } else {
+        (<any>check).checked = false
+      }
+      if(!(<any>ev).detail.checked && !this.termsRead){
+        this.presentAlert()
+      }
+    });
   }
 
   //Añadimos los validadores al form (el último del mail significa que ha de tener algo antes y después del @ y ha de acabar en . y 2-4 carácteres)
-  checkForm() {
+  async checkForm() {
     this.registerForm = this.fb.group({
       email: [
         '',
@@ -36,6 +57,7 @@ export class RegisterComponent implements OnInit {
       ],
       password: ['', Validators.required],
       username: ['', Validators.required],
+      termsAgreed: [false, Validators.requiredTrue]
     });
   }
 
@@ -56,4 +78,35 @@ export class RegisterComponent implements OnInit {
     })
     loading.present()
   }
+
+  // onConditionModal(){
+  //   this.presentAlert()
+  // }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Términos y Condiciones de uso',
+      buttons: [
+        {
+          text: 'Cancelar',
+          // role: 'cancel',
+          handler: async() => {
+            alert.dismiss()
+          }
+        },
+        {
+          text: 'Acepto',
+          // role: 'confirm',
+          handler: async () => {
+            this.termsRead = true
+            alert.dismiss()
+          },
+        },
+      ],
+      message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus orci, bibendum quis nisl ut, tincidunt maximus lorem. Nam nec tincidunt ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur eget porttitor urna. Proin ornare egestas tempus. Quisque vel sodales sapien. Aenean eget lobortis nulla.'
+    });
+
+    await alert.present();
+  }
 }
+
+
